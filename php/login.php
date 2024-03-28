9c4f74c451c8fb941ab43c60f1c96e4fe68a2d4c
Éllen Dias Farias <145167118+ellendias01@users.noreply.github.com>
2024-03-28 10:50:01 -0300

    Add files via upload

---
+++ php/login.php
@@ -0,0 +1,9 @@
+<?php
+
+$host = "localhost"; // Host name 
+$db_username = "id21939473_ellenly"; // Mysql username 
+$db_password = "Wangxian_13"; // Mysql password 
+$db_name = "id21939473_jogocandy"; // Database name 
+$db_table = "pontos"; // Table name
+
+?>
